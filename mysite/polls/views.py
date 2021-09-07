8b5a804bbb530c027cf9e73366b53a60ac6683e4
Nagesh Karmali <nags.ides@gmail.com>
2021-09-07 13:22:59 +0530

    using templates and index.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,21 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    #output = ','.join([q.question_text for q in latest_question_list])
+    context = {
+            'latest_question_list': latest_question_list,
+    }
+    #return HttpResponse(output)
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id)
@@ -13,6 +27,4 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
