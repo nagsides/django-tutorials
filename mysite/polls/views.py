91338981b347affe31d8a9946708cc83bfc91768
Nagesh Karmali <nags.ides@gmail.com>
2021-09-09 11:54:16 +0530

    added minimal form

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.http import Http404
+#from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 #from django.template import loader
 
 
