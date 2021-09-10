308e2da37fef2b51cb3400d3e1f46c39d3300241
Nagesh Karmali <nags.ides@gmail.com>
2021-09-10 18:49:54 +0530

    failing tests for views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
 #from django.http import Http404
+from django.utils import timezone
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -12,11 +13,13 @@ from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+    #template_name = 'polls/index.html'
+    #context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
