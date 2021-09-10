194081b1bf83a251f578dbd13e0b1288e366a9e6
Nagesh Karmali <nags.ides@gmail.com>
2021-09-10 19:01:32 +0530

    error resolved: template_name and context_object_name required for IndexView

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,8 +13,8 @@ from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
-    #template_name = 'polls/index.html'
-    #context_object_name = 'latest_question_list'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         return Question.objects.filter(
